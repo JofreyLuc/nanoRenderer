c3fa40ae53bd538ad413d5fc2b2da7204d0a8cdf
Jofrey Luc <luc.jofrey@yahoo.fr>
2017-03-17 08:52:49 +0100

    script python

--- src/homeexample.cpp
+++ src/homeexample.cpp
@@ -1,32 +1,18 @@
 #include "tgaimage.h"
-#include <cstdlib>
+#include "model.h"
+#include "geometry.h"
+#include <vector>
 #include <cmath>
+#include <cstdlib>
+#include <iostream>
+
+const TGAColor white = TGAColor(255, 255, 255, 255);
+const TGAColor red = TGAColor(255, 0, 0, 255);
+Model *model = NULL;
+const int width  = 1200;
+const int height = 1200;
 
 void line(int x0, int y0, int x1, int y1, TGAImage &image, TGAColor color) { 
-  bool steep = false; 
-  if (std::abs(x0-x1)<std::abs(y0-y1)) { // if the line is steep, we transpose the image 
-    std::swap(x0, y0); 
-    std::swap(x1, y1); 
-    steep = true; 
-  }
-    
-  if (x0>x1) { // make it left−to−right 
-    std::swap(x0, x1); 
-    std::swap(y0, y1); 
-  }
-    
-  for (int x=x0; x<=x1; x++) { 
-    float t = (x-x0)/(float)(x1-x0); 
-    int y = (int)round(y0*(1.-t) + y1*t); 
-    if (steep) { 
-      image.set(y, x, color); // if transposed, de−transpose 
-    } else { 
-      image.set(x, y, color); 
-    } 
-  } 
-}
-
-void line2(int x0, int y0, int x1, int y1, TGAImage &image, TGAColor color) { 
   bool steep = false; 
   if (std::abs(x0-x1)<std::abs(y0-y1)) { 
     std::swap(x0, y0); 
@@ -39,8 +25,8 @@ void line2(int x0, int y0, int x1, int y1, TGAImage &image, TGAColor color) {
   } 
   int dx = x1-x0; 
   int dy = y1-y0; 
-  float derror = std::abs(dy/float(dx)); 
-  float error = 0; 
+  int derror2 = std::abs(dy)*2; 
+  int error2 = 0; 
   int y = y0; 
   for (int x=x0; x<=x1; x++) { 
     if (steep) { 
@@ -48,27 +34,40 @@ void line2(int x0, int y0, int x1, int y1, TGAImage &image, TGAColor color) {
     } else { 
       image.set(x, y, color); 
     } 
-    error += derror; 
-    if (error>.5) { 
+    error2 += derror2; 
+    if (error2 > dx) { 
       y += (y1>y0?1:-1); 
-      error -= 1.; 
+      error2 -= dx*2; 
     } 
   } 
 } 
 
 int main(int argc, char** argv){
-  TGAColor red = TGAColor(255, 0, 0, 255);
-  TGAColor blue = TGAColor(0, 0, 255, 255);
-  TGAColor green = TGAColor(0, 255, 0, 255);
-  TGAColor white = TGAColor(255, 255, 255, 255);
-  TGAImage i = TGAImage(100, 100, TGAImage::RGB);
-  line2(5, 5, 50, 50, i, red);
-  line2(50, 50, 5, 5, i, blue);
-  line2(5, 5, 5, 75, i, green);
-  line2(13, 20, 80, 40, i, white); 
-  line2(20, 13, 40, 80, i, red); 
-  line2(80, 40, 13, 20, i, red);
-  i.flip_vertically();
-  i.write_tga_file("output.tga");
+
+  model = new Model(argv[1]);
+  
+  TGAImage image = TGAImage(width, height, TGAImage::RGB);
+
+  //Pour toutes les faces
+  for (int i=0; i<model->nfaces(); i++) { 
+    std::vector<int> face = model->face(i);
+    //Pour les trois vecteurs du triangle
+    for (int j=0; j<3; j++) {
+      //On récupère les coordonnées du début et de la fin
+      Vec3f v0 = model->vert(face[j]);
+      Vec3f v1 = model->vert(face[(j+1)%3]); 
+      int x0 = (v0.x+1.)*width/2.; 
+      int y0 = (v0.y+1.)*height/2.; 
+      int x1 = (v1.x+1.)*width/2.; 
+      int y1 = (v1.y+1.)*height/2.; 
+      line(x0, y0, x1, y1, image, red); 
+    } 
+  }
+
+  image.flip_vertically();
+  image.write_tga_file("output.tga");
+
+  delete model;
+
   return 0;
 }
